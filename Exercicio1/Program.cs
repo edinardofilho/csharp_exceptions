62d521e88b46d7c0ef8c0ce443b374f084d8c878
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-11-05 13:32:42 -0300

    Initial Commit

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Exercicio1.Entities;
+using Exercicio1.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            try
+            {
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+
+                Console.WriteLine(account);
+            } catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            } catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
